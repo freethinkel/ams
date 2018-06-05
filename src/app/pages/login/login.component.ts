f1efd6541b944a326e1c7b8c1d5a6be2a65e217b
freethinkel <ascuns05@icloud.com>
2018-06-05 11:40:46 +0300

    add new color

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../../services/api.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private apiService: ApiService) { }
 
   ngOnInit() {
   }
 
+  test() {
+    this.apiService.get('api/application/').subscribe((data) => {
+      console.log(data);
+    })
+  }
 }
